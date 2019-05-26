21c9bed572848957800909592cc78ad9ed8a24ba
jeanambeau <ambeaujean@gmail.com>
2019-05-26 19:20:25 -0400

    Add existing file

---
+++ README.md
@@ -0,0 +1,2 @@
+# The Hunchback of Notre Dame
+In fifteenth century Paris, the brother of the archdeacon plots with the gypsy king to foment a peasant revolt. Meanwhile, a freakish hunchback falls in love with the gypsy queen.
---
+++ www/js/app.js
@@ -0,0 +1,230 @@
+angular.module("the_hunchback_of_notre_dame", ["ngCordova","ionic","ionMdInput","ionic-material","ion-datetime-picker","ionic.rating","utf8-base64","angular-md5","chart.js","pascalprecht.translate","tmh.dynamicLocale","the_hunchback_of_notre_dame.controllers", "the_hunchback_of_notre_dame.services"])
+	.run(function($ionicPlatform,$window,$interval,$timeout,$ionicHistory,$ionicPopup,$state,$rootScope){
+
+		$rootScope.appName = "The Hunchback of Notre Dame" ;
+		$rootScope.appLogo = "data/images/header/logo.png" ;
+		$rootScope.appVersion = "1.0" ;
+		$rootScope.headerShrink = false ;
+
+		$rootScope.liveStatus = "pause" ;
+		$ionicPlatform.ready(function(){
+			$rootScope.liveStatus = "run" ;
+		});
+		$ionicPlatform.on("pause",function(){
+			$rootScope.liveStatus = "pause" ;
+		});
+		$ionicPlatform.on("resume",function(){
+			$rootScope.liveStatus = "run" ;
+		});
+
+
+		$rootScope.hide_menu_about_us = false ;
+		$rootScope.hide_menu_rate_this_app = false ;
+
+
+		$ionicPlatform.ready(function() {
+
+			localforage.config({
+				driver : [localforage.WEBSQL,localforage.INDEXEDDB,localforage.LOCALSTORAGE],
+				name : "the_hunchback_of_notre_dame",
+				storeName : "the_hunchback_of_notre_dame",
+				description : "The offline datastore for The Hunchback of Notre Dame app"
+			});
+
+			if(window.cordova){
+				$rootScope.exist_cordova = true ;
+			}else{
+				$rootScope.exist_cordova = false ;
+			}
+			//required: cordova plugin add ionic-plugin-keyboard --save
+			if(window.cordova && window.cordova.plugins.Keyboard) {
+				cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+				cordova.plugins.Keyboard.disableScroll(true);
+			}
+
+			//required: cordova plugin add cordova-plugin-statusbar --save
+			if(window.StatusBar) {
+				StatusBar.styleDefault();
+			}
+
+
+		});
+		$ionicPlatform.registerBackButtonAction(function (e){
+			if($ionicHistory.backView()){
+				$ionicHistory.goBack();
+			}else{
+				$state.go("the_hunchback_of_notre_dame.dashboard");
+			}
+			e.preventDefault();
+			return false;
+		},101);
+	})
+
+
+	.filter("to_trusted", ["$sce", function($sce){
+		return function(text) {
+			return $sce.trustAsHtml(text);
+		};
+	}])
+
+	.filter("trustUrl", function($sce) {
+		return function(url) {
+			return $sce.trustAsResourceUrl(url);
+		};
+	})
+
+	.filter("trustJs", ["$sce", function($sce){
+		return function(text) {
+			return $sce.trustAsJs(text);
+		};
+	}])
+
+	.filter("strExplode", function() {
+		return function($string,$delimiter) {
+			if(!$string.length ) return;
+			var $_delimiter = $delimiter || "|";
+			return $string.split($_delimiter);
+		};
+	})
+
+	.filter("strDate", function(){
+		return function (input) {
+			return new Date(input);
+		}
+	})
+	.filter("phpTime", function(){
+		return function (input) {
+			var timeStamp = parseInt(input) * 1000;
+			return timeStamp ;
+		}
+	})
+	.filter("strHTML", ["$sce", function($sce){
+		return function(text) {
+			return $sce.trustAsHtml(text);
+		};
+	}])
+	.filter("strEscape",function(){
+		return window.encodeURIComponent;
+	})
+	.filter("strUnscape", ["$sce", function($sce) {
+		var div = document.createElement("div");
+		return function(text) {
+			div.innerHTML = text;
+			return $sce.trustAsHtml(div.textContent);
+		};
+	}])
+
+	.filter("stripTags", ["$sce", function($sce){
+		return function(text) {
+			return text.replace(/(<([^>]+)>)/ig,"");
+		};
+	}])
+
+	.filter("chartData", function(){
+		return function (obj) {
+			var new_items = [];
+			angular.forEach(obj, function(child) {
+				var new_item = [];
+				var indeks = 0;
+				angular.forEach(child, function(v){
+						if ((indeks !== 0) && (indeks !== 1)){
+							new_item.push(v);
+						}
+						indeks++;
+					});
+					new_items.push(new_item);
+				});
+			return new_items;
+		}
+	})
+
+	.filter("chartLabels", function(){
+		return function (obj){
+			var new_item = [];
+			angular.forEach(obj, function(child) {
+			var indeks = 0;
+			new_item = [];
+			angular.forEach(child, function(v,l) {
+				if ((indeks !== 0) && (indeks !== 1)) {
+					new_item.push(l);
+				}
+				indeks++;
+			});
+			});
+			return new_item;
+		}
+	})
+	.filter("chartSeries", function(){
+		return function (obj) {
+			var new_items = [];
+			angular.forEach(obj, function(child) {
+				var new_item = [];
+				var indeks = 0;
+				angular.forEach(child, function(v){
+						if (indeks === 1){
+							new_item.push(v);
+						}
+						indeks++;
+					});
+					new_items.push(new_item);
+				});
+			return new_items;
+		}
+	})
+
+
+
+.config(["$translateProvider", function ($translateProvider){
+	$translateProvider.preferredLanguage("en-us");
+	$translateProvider.useStaticFilesLoader({
+		prefix: "translations/",
+		suffix: ".json"
+	});
+	$translateProvider.useSanitizeValueStrategy("escapeParameters");
+}])
+
+
+.config(function(tmhDynamicLocaleProvider){
+	tmhDynamicLocaleProvider.localeLocationPattern("lib/ionic/js/i18n/angular-locale_{{locale}}.js");
+	tmhDynamicLocaleProvider.defaultLocale("en-us");
+})
+
+
+.config(function($stateProvider, $urlRouterProvider,$sceDelegateProvider,$httpProvider,$ionicConfigProvider){
+	try{
+		// Domain Whitelist
+		$sceDelegateProvider.resourceUrlWhitelist([
+			"self",
+			new RegExp('^(http[s]?):\/\/(w{3}.)?youtube\.com/.+$'),
+			new RegExp('^(http[s]?):\/\/(w{3}.)?w3schools\.com/.+$'),
+		]);
+	}catch(err){
+		console.log("%cerror: %cdomain whitelist","color:blue;font-size:16px;","color:red;font-size:16px;");
+	}
+	$stateProvider
+	.state("the_hunchback_of_notre_dame",{
+		url: "/the_hunchback_of_notre_dame",
+			abstract: true,
+			templateUrl: "templates/the_hunchback_of_notre_dame-side_menus.html",
+			controller: "side_menusCtrl",
+	})
+
+	.state("the_hunchback_of_notre_dame.hunchback", {
+		url: "/hunchback",
+		views: {
+			"the_hunchback_of_notre_dame-side_menus" : {
+						templateUrl:"templates/the_hunchback_of_notre_dame-hunchback.html",
+						controller: "hunchbackCtrl"
+					},
+			"fabButtonUp" : {
+						template: '',
+					},
+		}
+	})
+
+
+// router by user
+
+
+	$urlRouterProvider.otherwise("/the_hunchback_of_notre_dame/dashboard");
+});
